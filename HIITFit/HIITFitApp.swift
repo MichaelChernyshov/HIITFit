644d8e91fdbd3cd51fb28bcce0370abf5738d70a
Mikhail Chernyshov <chernyshov.michaele@icloud.com>
2023-10-03 13:56:22 +0200

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Mikhail Chernyshov on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
